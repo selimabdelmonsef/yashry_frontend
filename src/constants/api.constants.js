7bd6b100dc390b6781a091444c496707d86e2e8f
Selim <selimabdelmonsef@gmail.com>
2021-09-25 15:20:18 +0200

    feat(api.constants.js, category.constants.js): initialized constants for apis and categories

---
+++ src/constants/api.constants.js
@@ -0,0 +1,6 @@
+export const api = {
+    products_api: 'https://test-api.edfa3ly.io/product',
+    products_category_api: 'https://test-api.edfa3ly.io/product?categoryId={{categoryId}}',
+    products_rating_api: 'https://test-api.edfa3ly.io/product?rating={{rating}}',
+    products_category_rating_api: 'https://test-api.edfa3ly.io/product?categoryId={{categoryId}}&rating={{rating}}',
+}
---
+++ src/constants/category.constants.js
@@ -0,0 +1,22 @@
+export const CATEGORY = [
+    {
+        categoryId: 1,
+        department_name: "MAN"
+    },
+    {
+        categoryId: 2,
+        department_name: "WOMEN"
+    },
+    {
+        categoryId: 3,
+        department_name: "KIDS"
+    },
+    {
+        categoryId: 4,
+        department_name: "UNISEX"
+    },
+    {
+        categoryId: 5,
+        department_name: "HOME"
+    },
+]
