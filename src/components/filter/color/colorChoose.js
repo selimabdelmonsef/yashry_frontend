ea120cf646b2988d4c787965b4ff00e9d748ad33
Selim <selimabdelmonsef@gmail.com>
2021-09-26 20:22:50 +0200

    feat(checkbox.js, searchbar.js, colorChoose.js, filter.js): Implemented searchbar and function generic checkBox component

---
+++ src/components/filter/color/colorChoose.js
@@ -0,0 +1,32 @@
+import React, { useEffect } from 'react';
+import SearchBar from '../../searchBar/searchBar';
+import styles from './colorChoose.module.css';
+import { MyCheckBox } from '../../checkbox/checkbox';
+import {COLORS} from '../../../constants/colors.constants';
+
+const ColorChoose = () => {
+
+    const colors = COLORS.map((color)=>{
+        return {
+            label:color,
+            checked:false,
+            value:color
+        }
+    });
+    useEffect(()=>{
+console.log(colors);
+    },[colors])
+
+
+    return (
+        <div className={styles.chooseColor}>
+
+            Color
+            <SearchBar placeholder={"Enter Color"} />
+            <div className={styles.checkbox}>
+                <MyCheckBox items = {colors}/>
+            </div>
+        </div>
+    );
+}
+export default ColorChoose;
--- src/components/filter/price/price_range.js
+++ src/components/filter/price/price_range.js
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from 'react';
 import MultiRangeSlider from '../../multiRangeSlider/multiRangeSlider.js';
-import styles from './price_range.module.css';
 
 const PriceRange = () => {
 
@@ -8,11 +7,6 @@ const PriceRange = () => {
     const [minPriceRange, setMinPriceRange]=useState(100);
     useEffect(() => {
       }, [minPriceRange]);
-
-      const clearPriceRange=()=>{
-    setMinPriceRange(0);
-    console.log(minPriceRange);
-      }
 
     return (
 <div>
@@ -25,12 +19,6 @@ const PriceRange = () => {
     //   onChange={({ min, max }) => setMinPriceRange(min)}
 
     />
-    {/* <div className={styles.clearRange}>
-    <h7 onClick={()=>clearPriceRange()}className={styles.xStyle}>X </h7>
-    <h7 >CLEAR</h7>
-
-    </div> */}
-   
     </div>
 
 
